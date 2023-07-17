5f57576c95eee53e479649c7b0903e02427545c0
BrunoMundim <brunomundimfranco@gmail.com>
2023-07-17 15:01:09 -0300

    Home page

---
+++ src/main/java/br/com/mundim/CarRent/controller/HomeController.java
@@ -0,0 +1,18 @@
+package br.com.mundim.CarRent.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("Home");
+        return modelAndView;
+    }
+
+}
